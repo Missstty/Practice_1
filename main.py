73781ad4384119f26fcec754c0bbc8cbfe9b0b3e
Missstty <miltomskaya@gmail.com>
2025-10-18 18:24:18 +0300

    Добавлена feature_one в main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-# Изменено в person1 на macOS
+# ВЕРСИЯ PERSON1 - Новая функциональность
 print("Hello from macOS!")
+print("This feature was added by Person1")
+
+def feature_one():
+    return "Feature 1 from Person1"

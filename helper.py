90e4c7dbccebd1b7078de604b61ab631a48bfacf
Missstty <miltomskaya@gmail.com>
2025-10-18 18:00:13 +0300

    Добавлены функции goodbye и get_time

--- helper.py
+++ helper.py
@@ -1,2 +1,9 @@
 def greet(name):
     return f"Hello, {name}!"
+
+def goodbye(name):
+    return f"Goodbye, {name}!"
+
+def get_time():
+    from datetime import datetime
+    return datetime.now().strftime("%H:%M:%S")
